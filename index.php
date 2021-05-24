1678e087029ad2a6fd561e84bde1e71eb11af17c
Nguyen Hiep <84691883+hiepsock1@users.noreply.github.com>
2021-05-24 09:09:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/template.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/template.js"></script>
+	<title>Website Điện tử, Điện máy</title>
+</head>
+<body>
+<div class="wrapper">
+	<?php
+	include('quantri/modules/config.php');
+	include('modules/header.php');
+	include('modules/menu.php');
+	?>
+
+	<?php
+	include('modules/content.php');
+    include('modules/footer.php');
+	?>
+</div>
+</body>
+</html>
